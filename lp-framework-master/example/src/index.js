2c18c0980816f8abbe8b707c2114ce9e47565ab5
Ihor Tarasenko <tarasenko.igr@gmail.com>
2019-09-23 13:41:01 +0300

    install framework

--- lp-framework-master/example/src/index.js
+++ lp-framework-master/example/src/index.js
@@ -1,20 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import LpFramework, {LpFrameworkWrapper} from '../../dist/index'
-import './index.scss';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <LpFramework
-    resourceFile={require('./resources/languages.json')}
-    funnel_name={"test"}>
-    <LpFrameworkWrapper Component={App}/>
-  </LpFramework>,
-  document.getElementById('root')
-);
+import './index.css'
+import App from './App'
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<App />, document.getElementById('root'))
