be14877b49277784f8977a35c92863d1dae62afd
Ihor Tarasenko <tarasenko.igr@gmail.com>
2019-09-25 11:11:25 +0300

    add ready project

--- bcevolution-folder/src/components/TopSection/Regform/Regform.js
+++ bcevolution-folder/src/components/TopSection/Regform/Regform.js
@@ -7,21 +7,23 @@ import 'react-intl-tel-input/dist/main.css'
 import * as errorMessage from '../../../helpers/errorMessage'
 
 import { ReactComponent as Mark } from './excl.svg'
-import logo from '../../BottomSection/bcprofitmin.svg'
+import logo from '../../BottomSection/logo.png'
 
 
 export default class Regform extends Component {
     constructor(props) {
-        super(props)
+        super(props);
 
-        /*this.state = {
-            fname: this.props.form.first_name,
-            lname: this.props.form.last_name,
-            email: this.props.form.email,
+        this.state = {
+            first_name: "",
+            last_name: "",
+            email: "",
             check: false,
-            pass: this.props.form.password,
-            tel: this.props.form.phone_number,
-        }*/
+            password: "",
+            tel: "",
+            agree_1: true,
+            agree_2: true
+        };
 
         this.setTextInputRef = element => {
             this.currentForm = element;
@@ -35,104 +37,66 @@ export default class Regform extends Component {
 
     handleForward(e) {
         let form = e.target.parentElement;
-        let values = [];
-        
-        // Step 1
-        if (form.classList.contains('one')) {
 
-            let inputs = [...form.querySelectorAll('.inputfield')];
+        let paramsToValidate = {};
 
-            inputs.map((input, index) => {
-                if(input.value.length === 0) {
-                    errorMessage.errorMessage(input, 'Please fill this field');
-                } else if ((index === 0 || index === 1) && !validateInput.checkOnlyLetters(input.value)) {
-                    errorMessage.errorMessage(input, 'Use letters only');
-                } else if ((index === 2) && !validateInput.validateEmail(input.value)) {
-                    errorMessage.errorMessage(input, 'Invalid email format');
-                } else {
-                    switch (index) {
-                        case 0:
-                            values[0] = input.value;
-                            break;
-                        case 1:
-                            values[1] = input.value;
-                            break;
-                        case 2:
-                            values[2] = input.value;
-                            break;
-                    }
-                }
+        // Step 1 or 2
+        if(this.props.step === 1 || this.props.step === 2){
+            // Step 1
+            if(this.props.step === 1){
+                paramsToValidate = {
+                    email: this.state.email,
+                    first_name: this.state.first_name,
+                    last_name: this.state.last_name,
+                    agree_2: this.state.agree_2
+                };
+            }
+            // Step 2
+            else if (this.props.step === 2){
+                paramsToValidate = {
+                    password: this.state.password
+                };
+            }
 
-                if (values[0] && values[1] && values[2]) {
-                    this.setState({
-                        fname: values[0],
-                        lname: values[1],
-                        email: values[2],
-                    }, () => {
-                        if(window.sbidTracking){
-                            window.sbidTracking.settings.params.email = this.state.email;
-                            window.sbidTracking.settings.params.fname = this.state.fname;
-                            window.sbidTracking.settings.params.lname = this.state.lname;
-                        }
-                    });
-                    this.props.handleForward(values[0], values[1], values[2]);
-                    this.props.handleStep(this.props.step + 1);
-                }
-            })
+
+            let submitResponse = this.props.validateParams(paramsToValidate);
+
+            if (submitResponse.success) {
+                this.props.handleForward(paramsToValidate);
+                this.props.handleStep(this.props.step + 1);
+            }
+            else{
+                this.setState({
+                    errors: submitResponse.errors
+                })
+            }
         }
-
-        // Step 2
-        if (form.classList.contains('two')) {
-            this.setState({
-                fname: this.props.form.first_name,
-                lname: this.props.form.last_name,
-                email: this.props.form.email,
-            }, () => {
-                let reqs = [...form.querySelectorAll('li')];
-
-                if(reqs.every(req => req.classList.contains('ok'))){
-    
-                    this.setState({
-                        pass: form.querySelector('.pass').value, 
-                    }, () => {
-                        if(window.sbidTracking){
-                            window.sbidTracking.settings.params.password = true;
-                        }
-                        this.props.handlePassSync(this.state.pass);
-                    });
-
-                    this.props.handleStep(this.props.step + 1);
-                }
-            })
-        }
-
         // Step 3
-        if (form.classList.contains('three')) {
-            this.setState({
-                fname: this.props.form.first_name,
-                lname: this.props.form.last_name,
-                email: this.props.form.email,
-                pass: this.props.form.password,
-            }, () => {
-                let tel = form.querySelector('.tel');
-                let intel = form.querySelector('.intl-tel-input');
-    
-                if(tel.value.length === 0) {
-                    errorMessage.errorMessage(intel, 'Please fill this field');
-                } else if(!validateInput.checkOnlyNumbers(tel.value)) {
-                    errorMessage.errorMessage(intel, 'Use numbers only');
-                } else {
-                    this.setState({
-                        tel: tel.value
-                    }, () => {
-                        this.props.handleStep(this.props.step + 1);
-                        this.props.handleSubmit(this.state.fname, this.state.lname, this.state.email, this.state.tel, this.state.pass); 
-                    })
-                }
-            })
+        else if (this.props.step === 3){
+            let tel = form.querySelector('.tel');
+            let dialCode = document.getElementsByClassName(".selected-dial-code");
 
+            let phone_number = dialCode.innerHTML + tel.value;
+            phone_number = tel.value;
 
+            paramsToValidate = {
+                phone_number: phone_number
+            };
+
+            let submitResponse = this.props.validateParams(paramsToValidate);
+
+            if (submitResponse.success) {
+                this.props.handleStep(this.props.step + 1);
+                this.props.handleSubmit(paramsToValidate);
+
+            }
+            else{
+                this.setState({
+                    errors: submitResponse.errors
+                })
+            }
         }
+
     }
 
     handleBackwards(e) {
@@ -146,7 +110,7 @@ export default class Regform extends Component {
                     step.classList.remove('step');
                 }
             })
-        })
+        });
 
         this.props.handleStep(parseInt(e.target.getAttribute('index')));
     }
@@ -175,17 +139,37 @@ export default class Regform extends Component {
     }
 
     componentDidMount() {
-        let inputs = [...document.querySelectorAll('.inputfield')];
+        /*let inputs = [...document.querySelectorAll('.inputfield')];
 
         inputs.map(input => {
             input.addEventListener('change', this.handleSync);
-        })
+        })*/
     }
 
-    handleFirstStepChange = () => {
-        if (window.sbidTracking) {
-            window.sbidTracking.settings.params.start_edit = "1";
-        }
+    handleStepChange = (name, value) => {
+
+        console.log(name, value);
+        this.setState({[name]: value, errors: null}, () => {
+
+            console.log(this.state);
+
+            if(name === "password"){
+                let submitResponse = this.props.validateParams({
+                    password: value
+                });
+
+                if (!submitResponse.success) {
+                    this.setState({
+                        errors: submitResponse.errors
+                    })
+                }
+                else{
+                    this.setState({
+                        errors: null
+                    })
+                }
+            }
+        });
     };
 
     render() {
@@ -210,21 +194,30 @@ export default class Regform extends Component {
                     </div>
                     <div className='inner'>
                         <div className='form-wrapper one'>
-                            <input className="inputfield fname" type="text" name="fname" placeholder={languageManager.fname} onChange={this.handleFirstStepChange}/>
-                            <input className="inputfield lname" type="text" name="lname" placeholder={languageManager.lname} onChange={this.handleFirstStepChange}/>
-                            <input className="inputfield email" type="text" name="email" placeholder={languageManager.email} autoComplete='off' onChange={this.handleFirstStepChange}/>
+                            {this.state.errors && <div className="errors" style={{color: '#ff3215'}}>
+                                {this.state.errors[0]}
+                            </div>}
+                            <input className="inputfield fname" type="text" name="first_name" placeholder={languageManager.fname} onChange={(e) => this.handleStepChange(e.target.name, e.target.value)}/>
+                            <input className="inputfield lname" type="text" name="last_name" placeholder={languageManager.lname} onChange={(e) => this.handleStepChange(e.target.name, e.target.value)}/>
+                            <input className="inputfield email" type="text" name="email" placeholder={languageManager.email} autoComplete='off' onChange={(e) => this.handleStepChange(e.target.name, e.target.value)}/>
                             <button onClick={this.handleForward.bind(this)} className='start'>{languageManager.button}</button>
                         </div>
                         <div className='form-wrapper two'>
-                            <input className="inputfield pass" type="password" maxLength="10" onChange={validateInput.checkInput} name="password" placeholder={languageManager.pass}/>
-                            <ul className='req'>
+                            {this.state.errors && <div className="errors" style={{color: '#ff3215'}}>
+                                {this.state.errors[0]}
+                            </div>}
+                            <input className="inputfield pass" type="password" maxLength="10" onChange={(e) => this.handleStepChange(e.target.name, e.target.value)} name="password" placeholder={languageManager.pass}/>
+                            {/*<ul className='req'>
                                 {languageManager.passtest.map(li => {
                                     return (<li key={li}>{li}</li>)
                                 })}
-                            </ul>
+                            </ul>*/}
                             <button onClick={this.handleForward.bind(this)} className='start'>{languageManager.button}</button>
                         </div>
                         <div className='form-wrapper three'>
+                            {this.state.errors && <div className="errors" style={{color: '#ff3215'}}>
+                                {this.state.errors[0]}
+                            </div>}
                             <IntlTelInput
                                 preferredCountries={[this.props.countryCode]}
                                 containerClassName="intl-tel-input"
--- bcevolution-folder/src/components/TopSection/TopSection.js
+++ bcevolution-folder/src/components/TopSection/TopSection.js
@@ -65,7 +65,7 @@ export default class TopSection extends Component {
                             <div className="col-lg-4 col-md-5 col-sm-12">
                                 <div className="regform" ref={this.regPanel}>
                                     <div className="reg-title"><span>{languageManager.topreg1}</span><br/>{languageManager.topreg2}</div>
-                                    <Regform handlePassSync={this.props.handlePassSync} form={this.props.form} pageHandler={this.props.pageHandler} countryCode={this.props.countryCode} languageManager={this.props.languageManager} handleStep={this.props.handleStep} handleForward={this.props.handleForward} handleSubmit={this.props.handleSubmit} step={this.props.step}/>
+                                    <Regform validateParams={this.props.validateParams} form={this.props.form} pageHandler={this.props.pageHandler} countryCode={this.props.countryCode} languageManager={this.props.languageManager} handleStep={this.props.handleStep} handleForward={this.props.handleForward} handleSubmit={this.props.handleSubmit} step={this.props.step}/>
                                 </div>
                             </div>
                         </div>
