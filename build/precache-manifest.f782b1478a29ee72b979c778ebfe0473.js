76977a65f52c9f9317ca8eb593d6d9a32f002035
Ihor Tarasenko <tarasenko.igr@gmail.com>
2019-10-17 14:38:16 +0300

    add style for different browsers

---
+++ build/precache-manifest.f782b1478a29ee72b979c778ebfe0473.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9419b1a91f3cae5cd4192b8ff84c27aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a7d0c2671d1937f2eccc",
+    "url": "/static/css/2.3f3e8ba0.chunk.css"
+  },
+  {
+    "revision": "2dc4701fdd7e3b0d4720",
+    "url": "/static/css/main.5aafa9a0.chunk.css"
+  },
+  {
+    "revision": "a7d0c2671d1937f2eccc",
+    "url": "/static/js/2.3ff2b043.chunk.js"
+  },
+  {
+    "revision": "2dc4701fdd7e3b0d4720",
+    "url": "/static/js/main.9803d17a.chunk.js"
+  },
+  {
+    "revision": "3c207c1c3e899626d5fb",
+    "url": "/static/js/runtime~main.c95fceec.js"
+  },
+  {
+    "revision": "caf7474b0ee73d2b240030e40ee5f811",
+    "url": "/static/media/6b.caf7474b.png"
+  },
+  {
+    "revision": "569222d0e72efcece05bb4ab5ed0639d",
+    "url": "/static/media/Mercedes-Key.569222d0.jpg"
+  },
+  {
+    "revision": "37af8d8322b476dfd966e58d2534d936",
+    "url": "/static/media/bc.37af8d83.jpg"
+  },
+  {
+    "revision": "674ac87c20f4462d7f425e424f44bdbb",
+    "url": "/static/media/bg.674ac87c.jpg"
+  },
+  {
+    "revision": "94e0d9ea5d44ce5e2d9e069a0851840e",
+    "url": "/static/media/bitimg.94e0d9ea.jpg"
+  },
+  {
+    "revision": "d715f03590db6a9f7bf86bdc7b3c0b56",
+    "url": "/static/media/bitimg2.d715f035.jpg"
+  },
+  {
+    "revision": "dac52b6cf7697ee926b9277830698cb4",
+    "url": "/static/media/countries.dac52b6c.csv"
+  },
+  {
+    "revision": "4440bb4e2bdca24150e2e7e2534fb690",
+    "url": "/static/media/crypto-castle-san-francisco-1750.4440bb4e.jpg"
+  },
+  {
+    "revision": "887bd7344faf20266a51a9186c9f2cda",
+    "url": "/static/media/data.887bd734.jpg"
+  },
+  {
+    "revision": "0a13c39f890ad109579a104b7d4f03c0",
+    "url": "/static/media/first.0a13c39f.png"
+  },
+  {
+    "revision": "80a4ed4719ed2aa65879060f8926fb91",
+    "url": "/static/media/flags.80a4ed47.png"
+  },
+  {
+    "revision": "7de34bf24d00cb912b2b81bfefc10d03",
+    "url": "/static/media/flags@2x.7de34bf2.png"
+  },
+  {
+    "revision": "e3633059808726e92f8bcddf8a2fe45d",
+    "url": "/static/media/fours.e3633059.jpg"
+  },
+  {
+    "revision": "fe30b19bd0acbaf309c9b71c15f1c869",
+    "url": "/static/media/lp-en.fe30b19b.mp4"
+  },
+  {
+    "revision": "ebab478d8ba00df0495777762357cd7c",
+    "url": "/static/media/me-p-500.ebab478d.jpeg"
+  },
+  {
+    "revision": "baf262450f7f01228403f55f2ed313f0",
+    "url": "/static/media/members-en.baf26245.mp4"
+  },
+  {
+    "revision": "c1b1296e1276bf0d6a80a7a0af27cc71",
+    "url": "/static/media/money.c1b1296e.jpg"
+  },
+  {
+    "revision": "88b806f543ca934380bb81a3b5737b0e",
+    "url": "/static/media/rolex.88b806f5.jpeg"
+  },
+  {
+    "revision": "2cbde4f70a1748b69137eb893fc776c5",
+    "url": "/static/media/safari.2cbde4f7.jpg"
+  },
+  {
+    "revision": "76acc44311892d1cbeefedd0c06bf1d3",
+    "url": "/static/media/second.76acc443.png"
+  },
+  {
+    "revision": "52b347e17316eb904eefe710b6eb70a1",
+    "url": "/static/media/shop.52b347e1.jpg"
+  },
+  {
+    "revision": "66e72eae852bd1c5360c4e0b892eab5b",
+    "url": "/static/media/shutterstock_1224656441-p-800.66e72eae.jpeg"
+  },
+  {
+    "revision": "0430a2135d9449ee99ad4c60f0d51e4e",
+    "url": "/static/media/snapbag.0430a213.jpeg"
+  },
+  {
+    "revision": "da5b7f4675b06fd314f61e6d168911f1",
+    "url": "/static/media/test231.da5b7f46.jpg"
+  },
+  {
+    "revision": "a898a997595f032464fec472f92ea0e2",
+    "url": "/static/media/third.a898a997.jpg"
+  },
+  {
+    "revision": "e23aa0e96419a75e83a65bcbca8cac7a",
+    "url": "/static/media/video-2.e23aa0e9.jpg"
+  }
+]);
